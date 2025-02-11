31357b265bed908afa45810a17e373faf61fdc72
MoonLightCodes <poornachander295@gmail.com>
2025-02-11 20:30:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-document.getElementById('inp').addEventListener('input', function() {
-  convertToRoman(this.value);
-});
 
-function convertToRoman(num) {
-  const obj = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
-  };
+        document.getElementById('inp').addEventListener('input', function() {
+            convertToRoman(this.value);
+        });
 
-  let roman = '';
-  for (let key in obj) {
-    while (num >= obj[key]) {
-      roman += key;
-      num -= obj[key];
-    }
-  }
-  document.getElementById('ot').innerHTML = roman;
-}
+        function convertToRoman(num) {
+            if (num === "" || num <= 0) {
+                document.getElementById('ot').innerHTML = "Please enter a valid positive number.";
+                return;
+            }
+
+            const obj = {
+                M: 1000,
+                CM: 900,
+                D: 500,
+                CD: 400,
+                C: 100,
+                XC: 90,
+                L: 50,
+                XL: 40,
+                X: 10,
+                IX: 9,
+                V: 5,
+                IV: 4,
+                I: 1
+            };
+
+            let roman = '';
+            for (let key in obj) {
+                while (num >= obj[key]) {
+                    roman += key;
+                    num -= obj[key];
+                }
+            }
+            document.getElementById('ot').innerHTML = roman;
+        }
